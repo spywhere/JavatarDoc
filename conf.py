bee123d94d3d38b18b7f8c4ccf60d0825aeca3ed
Sirisak Lueangsaksri <spywhere@me.com>
2014-04-28 12:44:27 +0700

    Use ReadTheDoc theme

--- conf.py
+++ conf.py
@@ -100,6 +100,12 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 html_theme = 'default'
 
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = ['_themes/']
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
